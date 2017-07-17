718a95568401aceba435640e90ac7697fe3c1e29
lancf123 <lanchuanfu@youngleader.co>
2017-07-17 21:22:53 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="height:100%;">
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+
+<script>
+import 'config/common.less'
+export default {}
+</script>
+
+<style lang="less">
+  @import '~vux/src/styles/reset.less';
+  @import '~vux/src/styles/1px.less';
+</style>
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import FastClick from 'fastclick'
+import 'babel-polyfill'
+import { sync } from 'vuex-router-sync'
+import App from './App'
+import router from './router'
+import store from './store'
+import {ToastPlugin,LoadingPlugin,WechatPlugin,AjaxPlugin,AlertPlugin} from 'vux'
+import AMap from 'vue-amap'
+
+Vue.use(ToastPlugin)    //使用提醒
+Vue.use(LoadingPlugin)  //使用Loading
+Vue.use(WechatPlugin)   //微信js-sdk
+Vue.use(AlertPlugin)    //alert的使用
+Vue.use(AjaxPlugin)     //调用axios的ajax请求
+Vue.use(AMap)           //高德地图的使用
+
+AMap.initAMapApiLoader({
+  key: 'f01650aa86f4858fa6d5e0b0a850cfa0',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor']
+})
+
+//路由和store同步
+sync(store, router)
+FastClick.attach(document.body)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
